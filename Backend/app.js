5a6d97d4583ec579ec36b1383c01edd9440bd52b
nishantshah021 <155080264+nishantshah021@users.noreply.github.com>
2025-12-10 01:46:05 +0545

    added authentication

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
 
 module.exports = app;
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/auth',require('./routes/auth'));
---
+++ Backend/models/Prospect.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const ProspectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    bloodGroup: {
+        type: String,
+        required: true,
+        enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 18,
+        max: 65,
+    },
+    lastDonationDate: {
+        type: Date,
+        required: true,
+    },
+    contactInfo: {
+        type: String,
+        required: true,
+    },
+    diseases: {
+        type: [String],
+        default: [],
+    },
+    bloodpressure: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['eligible', 'ineligible'],
+        default: 'eligible',
+    },
+},
+{
+    timestamps: true,
+    });
+    module.exports = mongoose.model('Prospect', ProspectSchema);
